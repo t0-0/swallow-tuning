ea575f30270af0db9629102fa9a55cf1e66e5b03
t0-0 <takumioka428@gmail.com>
2024-03-29 01:21:35 +0900

    add instruction scripts

--- scripts/swallow/instruction/swallow-7b-baseline_GB256_iter2000.sh
+++ scripts/swallow/instruction/swallow-7b-baseline_GB256_iter2000.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -l rt_AF=2
-#$ -l h_rt=0:5:00:00
+#$ -l h_rt=0:10:00:00
 #$ -j y
 #$ -o outputs/swallow-7b/instruction/
 #$ -cwd
@@ -69,7 +69,7 @@ SEQ_LENGTH=4096
 # checkpoint & tokenizer
 TOKENIZER_DIR=/bb/llm/gaf51275/llama/huggingface-checkpoint/Swallow-7b-hf
 CHECKPOINT_DIR=/bb/llm/gaf51275/llama/huggingface-checkpoint/Swallow-7b-hf
-CHECKPOINT_SAVE_DIR="/groups/gcc50623/acf15833kg/swallow-project/checkpoints/finetuning/Swallow-7b-VE-instruct-v1.0/baseline-lr_${LR}-minlr_${MIN_LR}-GB${GLOBAL_BATCH_SIZE}_iter${ITERATIONS}"
+CHECKPOINT_SAVE_DIR="/groups/gaf51217/acf15833kg/swallow-project/checkpoints/finetuning/Swallow-7b-VE-instruct-v1.0/baseline-lr_${LR}-minlr_${MIN_LR}-GB${GLOBAL_BATCH_SIZE}_iter${ITERATIONS}"
 
 mkdir -p ${CHECKPOINT_SAVE_DIR}
 
